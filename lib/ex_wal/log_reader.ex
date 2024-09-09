5fad884cf252e7b03ba01bb85c414abdb5de1ab0
wanqiang <wanqiang@shizhuang-inc.com>
2024-09-09 20:48:39 +0800

    Add doc for reader/writer

--- lib/ex_wal/log_reader.ex
+++ lib/ex_wal/log_reader.ex
@@ -1,4 +1,12 @@
 defprotocol ExWal.LogReader do
+  @moduledoc """
+  Reader reads a virtual WAL.
+  """
+
+  @doc """
+  next returns a binary in the next record. It returns io.EOF if there
+  are no more records.
+  """
   @spec next(t()) :: {:ok, bytes :: binary()} | :eof | {:error, reason :: term()}
   def next(impl)
 
--- lib/ex_wal/log_writer.ex
+++ lib/ex_wal/log_writer.ex
@@ -1,8 +1,20 @@
 defprotocol ExWal.LogWriter do
+  @moduledoc """
+  Writer writes to a virtual WAL. A Writer in standalone mode maps to a
+  single LogWriter. In failover mode, it can failover across multiple
+  physical log files.
+  """
+
+  @doc """
+  write_record writes a complete record. The record is asynchronously persisted to the underlying writer.
+  """
   @spec write_record(impl :: t(), bytes :: binary()) ::
           {:ok, written_bytes :: non_neg_integer()} | {:error, reason :: term()}
   def write_record(impl, bytes)
 
+  @doc """
+  stop stops the writer.
+  """
   @spec stop(impl :: t()) :: :ok
   def stop(impl)
 end
