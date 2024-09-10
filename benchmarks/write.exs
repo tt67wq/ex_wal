e2d40d09a777a2d320ce1b1b149ee8188e770a5a
wanqiang <wanqiang@shizhuang-inc.com>
2024-09-10 18:47:03 +0800

    Update write benchmark

--- benchmarks/write.exs
+++ benchmarks/write.exs
@@ -21,32 +21,33 @@ defmodule BenchmarkApp do
   use ExWal, otp_app: :bench
 end
 
+Application.put_env(:bench, BenchmarkApp, [])
+{:ok, _} = BenchmarkApp.start_link()
+{:ok, manager} = BenchmarkApp.manager(data_dir, :standalone)
+
 Benchee.run(
   %{
-    "write" => fn {_, writer, data} ->
-      ExWal.LogWriter.write_record(writer, data, timeout: 10000)
+    "write" => fn {writer, data} ->
+      ExWal.LogWriter.write_record(writer, data, timeout: 10_000)
     end
   },
   inputs: %{
-    "small value, nosync" => {small, []},
-    "small value" => {small, []},
-    "1KB value" => {one_kb, []},
-    "1MB value" => {one_mb, []},
-    "10MB value" => {ten_mb, []}
+    "small value" => {manager, small, [idx: 1]},
+    "1KB value" => {manager, one_kb, [idx: 2]},
+    "1MB value" => {manager, one_mb, [idx: 3]},
+    "10MB value" => {manager, ten_mb, [idx: 4]}
   },
-  before_scenario: fn {data, options} ->
-    Application.put_env(:bench, BenchmarkApp, options)
-    {:ok, _} = BenchmarkApp.start_link()
-    {:ok, manager} = BenchmarkApp.manager(data_dir, :standalone)
-    {:ok, writer} = ExWal.Manager.create(manager, 1)
-    {manager, writer, data}
+  before_scenario: fn {manager, data, options} ->
+    {:ok, writer} = ExWal.Manager.create(manager, options[:idx])
+    {writer, data}
   end,
-  before_each: fn {manager, writer, data} ->
-    {manager, writer, data}
+  before_each: fn {writer, data} ->
+    {writer, data}
   end,
-  after_scenario: fn {manager, writer, _} ->
+  after_scenario: fn {writer, _} ->
     ExWal.LogWriter.stop(writer)
-    ExWal.Manager.close(manager)
-    cleanup.()
   end
 )
+
+ExWal.Manager.close(manager)
+cleanup.()
